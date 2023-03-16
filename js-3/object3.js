1624c10df0c758c2f5faf6b61629146b96274d95
Exanimum18 <santiago.baron.18.com>
2023-03-15 19:59:52 -0500

    Organizando el codigo

--- js-3/object3.js
+++ js-3/object3.js
@@ -7,4 +7,11 @@ receta.ingredientes = [{nombre: "Pan", cantidad: 2}];
 receta.ingredientes.push({nombre: "Queso", cantidad: 1})
 
 console.log(receta.ingredientes[0].nombre);
-console.log(receta.ingredientes[0].cantidad + receta.ingredientes[1].cantidad);
+
+let resultado = 0
+for (let i = 0; i < receta.ingredientes.length; i++) {
+    const cantidad = receta.ingredientes[i].cantidad;
+    resultado += cantidad
+}
+
+console.log(resultado);
